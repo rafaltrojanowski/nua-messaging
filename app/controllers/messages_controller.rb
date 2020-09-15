c55799bea347971bf97c32d37e27718fa674f2da
Rafał Trojanowski <rt.trojanowski@gmail.com>
2020-09-15 10:16:38 +0200

    Patients should have the ability to send a message to their doctor through the app
    - Add create message action
    - Add model validation
    - Display errors in form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,4 +8,20 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
+  def create
+    @message = Message.new(message_params.merge(
+      outbox: User.current.outbox,
+      inbox: User.default_doctor.inbox,
+    ))
+    if @message.save
+      redirect_to @message
+    else
+      render "new"
+    end
+  end
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+
 end
